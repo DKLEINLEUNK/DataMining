67592238edc565a233a13de3fccc7dc6e5f49e39
DKLEINLEUNK <d.kleinleunk@gmail.com>
2024-05-17 17:26:11 +0200

    Extended random forest to include ensembling

--- Assignment 2/ensembling/bagging.py
+++ Assignment 2/ensembling/bagging.py
@@ -1,17 +1,19 @@
-""" Random Forest Classifier """
+""" Bagging ensemble method for Random Forest Classifier """
 
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.ensemble import BaggingClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 
 PATH_TRAIN = "/home/david/Studies/Data Mining Techniques/repository/Assignment 2/data/train_cleaned_100_000.csv"
 PATH_TEST = "/home/david/Studies/Data Mining Techniques/repository/Assignment 2/data/test_cleaned_100_000.csv"
-PATH_EXPORT = "/home/david/Studies/Data Mining Techniques/repository/Assignment 2/pending_submissions/random_forest.csv"
+PATH_EXPORT = "/home/david/Studies/Data Mining Techniques/repository/Assignment 2/pending_submissions/bagging.csv"
 
-# Read the CSV files
+# Read the CSV file
 df_train = pd.read_csv(PATH_TRAIN)
 df_test = pd.read_csv(PATH_TEST)
 print("Data read successfully")
@@ -22,10 +24,11 @@ y_train = df_train['booking_bool']
 
 # Algorithm
 clf = RandomForestClassifier(n_estimators=100, random_state=42)  # TODO - Try different values for n_estimators on the real dataset
-clf.fit(X_train, y_train)
+bagging = BaggingClassifier(clf, max_samples=0.5, max_features=0.5)  # TODO - Try different values for max_samples and max_features (probably needs to be a bit lower)
+bagging.fit(X_train, y_train)
 
 # Rank & sort the results
-y_pred = clf.predict(df_test)
+y_pred = bagging.predict(df_test)
 # np.argmax(y_pred)  # NOTE - I don't think this line is doing anything
 ranked_results = np.argsort(-y_pred)
 sorted_test = df_test.iloc[ranked_results]
