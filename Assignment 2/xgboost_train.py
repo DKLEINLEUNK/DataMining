fad49e185dca06c2cb909561ec7a16d3a27a9644
kaleem-01 <kaleemullah@student.uva.nl>
2024-05-19 11:16:59 +0200

    pushing all changes

--- Assignment 2/xgboost_train.py
+++ Assignment 2/xgboost_train.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import classification_report, confusion_matrix
-from imblearn.over_sampling import SMOTE
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.ensemble import BalancedRandomForestClassifier
+from cuml import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from cuml.metrics import classification_report, confusion_matrix
+# from imblearn.over_sampling import SMOTE
+# from imblearn.under_sampling import RandomUnderSampler
+# from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.impute import SimpleImputer
-from imblearn.over_sampling import SMOTE
+# from imblearn.over_sampling import SMOTE
 import xgboost as xgb
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from cuml.metrics import accuracy_score, confusion_matrix
 from datetime import datetime
 import time
 import dask.dataframe as dd
@@ -21,7 +21,6 @@ from helper import timing_decorator
 logging.basicConfig(filename='xgboost_model.log', level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-logging.info("Used data that was not scaled")
 
 @timing_decorator
 def xg_boost(data, test=None, save_df=False, output=False):
@@ -37,13 +36,17 @@ def xg_boost(data, test=None, save_df=False, output=False):
     - fitted_results: Series, the predictions made on the test set.
     - 
     """
-    # Set XGBoost parameters
     params = {
         'objective': 'binary:logistic',
-        'max_depth': 6,
+        'max_depth': 5,
         'eta': 0.3,
-        'eval_metric': 'logloss'
+        'eval_metric': 'logloss',
+        'n_estimators': 10,
+        'silent': True,
+        'verbose_eval': True,
+        'tree_method': 'gpu_hist',
     }
+    # Set XGBoost parameters
 
     if test is not None:
         X_train = data.drop(columns=["srch_id", 'booking_bool', "click_bool", "position"])
@@ -84,19 +87,18 @@ def xg_boost(data, test=None, save_df=False, output=False):
         np.argmax(fitted_results)
         # Rank the results
         ranked_results = np.argsort(-fitted_results)
-        
         # Evaluate the model
         y_pred = (fitted_results > 0.5).astype(int)
         accuracy = accuracy_score(y_test, y_pred)
         conf_matrix = confusion_matrix(y_test, y_pred)
-        class_report = classification_report(y_test, y_pred)
+        # class_report = classification_report(y_test, y_pred)
         print("Accuracy:", accuracy)
         print("Confusion Matrix:\n", conf_matrix)
-        print("Classification Report:\n", class_report)
+        # print("Classification Report:\n", class_report)
         
         logging.info(f"Accuracy: {accuracy}")
         logging.info(f"Confusion Matrix:\n{conf_matrix}")
-        logging.info(f"Classification Report:\n{class_report}")
+        # logging.info(f"Classification Report:\n{class_report}")
 
         
     
@@ -107,21 +109,40 @@ def xg_boost(data, test=None, save_df=False, output=False):
         submission_df = pd.DataFrame({'srch_id': sorted_test['srch_id'], 'prop_id': sorted_test['prop_id']})
         submission_df.head(10)
         submission_df.to_csv(f"pending_submissions\\xgboost_scaled_{save_df}.csv", index=False)
-    
+
     
     if output:
         return fitted_results, bst
-    
+
+
 if __name__ == "__main__":
-    os.chdir("C:\\Users\\Kaleem\\Documents\\Courses\\Data Mining\\DataMining\\Assignment 2")
-    # Load the data
-    data = pd.read_csv("data\\train_cleaned.csv", nrows=100_000)
+    import cudf
+    import dask_cudf
+
+    # from datetime import datetime
+    start_time = datetime.now()
+    logging.info("Loading and training (with CUDA)")
+    # os.chdir("C:\\Users\\Kaleem\\Documents\\Courses\\Data Mining\\DataMining\\Assignment 2")
+    PATH_train = "/windows/Users/Kaleem/Documents/Courses/Data Mining/DataMining/Assignment 2/data/train_cleaned.csv"
+    PATH_test = "/windows/Users/Kaleem/Documents/Courses/Data Mining/DataMining/Assignment 2/data/test_cleaned.csv"
+
+    data = pd.read_csv(PATH_train)
+
+    # data = dask_cudf.from_cudf(cudf.read_csv(PATH_train), npartitions=100).compute()
+    # data = data.persist()
+    # data = pd.read_csv(PATH_train)
     # xg_boost(data)
-    test = pd.read_csv("data\\test_cleaned.csv", nrows=100_000)
-    xg_boost(data, test, save_df='updated')
-    new_df = pd.read_csv("pending_submissions\\xgboost_scaled_updated.csv")
-    if len(new_df) == 4959183:
-        print("Submission file matches requirements")
+    xg_boost(data, test, save_df="sorted")
+    # Load the data
+    # test = pd.read_csv(PATH_train)
+    # xg_boost(data)
+    # xg_boost(data, test, save_df='updated')
+    # new_df = pd.read_csv("pending_submissions\\xgboost_scaled_updated.csv")
+    # if len(new_df) == 4959183:
+    #     print("Submission file matches requirements")
+    end_time = datetime.now()
+    print(f"Time taken: {end_time - start_time}")
+    logging.info(f"Time taken: {end_time - start_time}")
 
 ############################################################################################
 ### Accuracy measures
